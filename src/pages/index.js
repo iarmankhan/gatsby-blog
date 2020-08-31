da3f2b3d30a9e97b5a3641738290d7de7f0ae034
iarmankhan <armankhan9244@gmail.com>
2020-08-31 17:06:02 +0530

    Added plugin for mdx file, added siteMetadata hook, added dummy posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import {useSiteMetadata} from '../hooks/useSiteMetadata'
 
-export default () => {
-    return <h1 > Hello World! < /h1>;
+const App = () => {
+    const { title, description } = useSiteMetadata();
+    return (
+        <>
+        <h1>{title}</h1>
+        <p>{description}</p>
+        </>
+    );
 };
+
+export default App
