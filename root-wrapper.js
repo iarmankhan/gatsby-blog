2eaad11c05704968a2651e2a9a05c128515a743e
iarmankhan <armankhan9244@gmail.com>
2020-09-02 14:02:10 +0530

    Added code syntax highlighting

--- root-wrapper.js
+++ root-wrapper.js
@@ -1,5 +1,7 @@
 import {MDXProvider} from '@mdx-js/react';
 import React from "react";
+import theme from 'prism-react-renderer/themes/nightOwl';
+import Highlight, {defaultProps} from 'prism-react-renderer';
 
 const components = {
     h2: ({children}) => (
@@ -8,6 +10,40 @@ const components = {
     'p.inlineCode': props => (
         <code style={{backgroundColor: 'lightgray'}} {...props} />
     ),
+    pre: props => {
+        const className = props.children.props.className || '';
+        const matches = className.match(/language-(?<lang>.*)/);
+        return (
+            <Highlight
+                {...defaultProps}
+                code={props.children.props.children.trim()}
+                language={
+                    matches && matches.groups && matches.groups.lang
+                        ? matches.groups.lang
+                        : ''
+                }
+                theme={theme}>
+                {({
+                      className,
+                      style,
+                      tokens,
+                      getLineProps,
+                      getTokenProps,
+                  }) => (
+                    <pre className={className} style={style}>
+                          {tokens.map((line, i) => (
+                              <div {...getLineProps({line, key: i})}>
+                                  {line.map((token, key) => (
+                                      <span {...getTokenProps({token, key})} />
+                                  ))}
+                              </div>
+                          ))}
+                    </pre>
+                )
+                }
+            </Highlight>
+        )
+    },
 };
 
 export const wrapRootElement = ({element}) => (
