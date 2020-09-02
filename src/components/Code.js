b574f9f1eec3d4ec8d82a257676fbc0df1db60e1
iarmankhan <armankhan9244@gmail.com>
2020-09-02 14:42:01 +0530

    Added copy code to clipboard functionality

--- src/components/Code.js
+++ src/components/Code.js
@@ -2,29 +2,47 @@ import React from "react";
 import Highlight, {defaultProps} from "prism-react-renderer";
 import theme from 'prism-react-renderer/themes/nightOwl'
 import styled from "styled-components";
+import {copyToClipboard} from "../utils/copy-to-clipboard";
 
 export const Pre = styled.pre`
-  text-align: left;
-  margin: 1em 0;
-  padding: 0.5em;
-  overflow-x: auto;
-  border-radius: 3px;
-
-  & .token-line {
-    line-height: 1.3em;
-    height: 1.3em;
-  }
-  font-family: 'Courier New', Courier, monospace;
+    position: relative;
+    text-align: left;
+    margin: 1em 0;
+    padding: 0.5em;
+    overflow-x: auto;
+    border-radius: 3px;
+    
+    & .token-line {
+        line-height: 1.3em;
+        height: 1.3em;
+    }
+    font-family: 'Courier New', Courier, monospace;
 `;
 
 export const LineNo = styled.span`
-  display: inline-block;
-  width: 2em;
-  user-select: none;
-  opacity: 0.3;
+    display: inline-block;
+    width: 2em;
+    user-select: none;
+    opacity: 0.3;
+`;
+
+const CopyCode = styled.button`
+    position: absolute;
+    right: 0.25rem;
+    border: 0;
+    border-radius: 3px;
+    margin: 0.25em;
+    opacity: 0.3;
+    &:hover {
+        opacity: 1;
+    }
 `;
 
 const Code = ({codeString, language}) => {
+    const handleClick = () => {
+        copyToClipboard(codeString);
+    };
+
     return (
         <Highlight
             {...defaultProps}
@@ -39,6 +57,7 @@ const Code = ({codeString, language}) => {
                   getLineProps,
                   getTokenProps,
               }) => (<Pre className={className} style={style}>
+                        <CopyCode onClick={handleClick}>Copy</CopyCode>
                           {tokens.map((line, i) => (
                               <div {...getLineProps({line, key: i})}>
                                   <LineNo>{i + 1}</LineNo>
